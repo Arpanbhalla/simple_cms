8b4fb2751cc51982899388e9dfe04a6972fd7b24
Arpan Bhalla <arpanbhalla@192-168-1-5.tpgi.com.au>
2017-05-20 21:20:11 +1000

    CRUD ->Pages Done

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ resources :subjects do
   end
 end
 
+  resources :pages do
+  member do
+    get :delete
+  end
+end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
