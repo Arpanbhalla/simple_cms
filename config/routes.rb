9a8df706c7057d8a2e88e76a76d0cc40ffa8d960
Arpan Bhalla <arpanbhalla@gmail.com>
2017-05-23 07:52:25 +1000

    New login controller Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/delete'
+
   get 'admin', :to => 'access#menu'
   get 'access/menu'
   post 'access/login'
